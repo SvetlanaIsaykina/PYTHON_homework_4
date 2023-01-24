380cf86796b135bec392f589ce4f877c253a5a67
Svetlana Isaykina <svet4731@gmail.com>
2023-01-24 21:14:31 +0300

    task 1

--- task_4_1.py
+++ task_4_1.py
@@ -1,7 +1,16 @@
 # Задача 1. Задайте натуральное число N. Напишите
-# программу, которая составит список простых множителей
-# числа N.
+# программу, которая составит список простых множителей числа N.
 
+number_n = int(input('Введите число N: '))
+print(number_n)
 
+prime_factor_list = []
 
+factor = 2
+while number_n > 1:
+    while number_n % factor == 0:
+        prime_factor_list.append(factor)
+        number_n //= factor
+    factor += 1
 
+print(f'Простые множители - {prime_factor_list}')
